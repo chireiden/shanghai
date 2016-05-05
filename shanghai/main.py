1ac6e12a51d8ca92d7b871ba929316fb9afcae2b
Lars Peter Søndergaard <nitori@chireiden.net>
2016-05-05 14:26:50 +0200

    Try to use uvloop as event loop if available

--- shanghai/main.py
+++ shanghai/main.py
@@ -7,6 +7,16 @@ from .network import Network
 
 
 def main():
+    try:
+        import uvloop
+    except ImportError:
+        # TODO: use a proper terminal color tool in the future (e.g. colorama)
+        # ... and possibly hook it up with the logging module.
+        print('\033[32;1mUsing default event loop.\033[0m')
+    else:
+        print('\033[32;1mUsing uvloop event loop.\033[0m')
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
     loop = asyncio.get_event_loop()
     queue = asyncio.Queue(100)
     bot = Shanghai()
