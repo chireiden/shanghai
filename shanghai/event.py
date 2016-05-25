ca0f4fafe3682a68b02be6d0c44d12241cc88725
FichteFoll <fichtefoll2@googlemail.com>
2016-05-25 05:57:53 +0200

    Simply Event class

--- shanghai/event.py
+++ shanghai/event.py
@@ -1,15 +1,4 @@
+from collections import namedtuple
 
-
-class Event:
-    """Sample Event class
-
-    Carry an event name/typo and the message if available."""
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    def __repr__(self):
-        if self.value:
-            return 'Event({!r}, {!r})'.format(self.name, self.value)
-        return 'Event({!r})'.format(self.name)
+Event = namedtuple("Event", "name value")
+Event.__new__.__defaults__ = (None,)  # Make last argument optional
