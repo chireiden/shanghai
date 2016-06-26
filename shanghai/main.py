35f3aec2a3aba66d1dc4a433471d0d9ab0520dda
FichteFoll <fichtefoll2@googlemail.com>
2016-06-26 20:23:46 +0200

    Read commands from stdin and send them to networks
    Through the additional and always active task, we can also close
    immediately on Ctrl+C now.
    
    Updates #14.

--- shanghai/main.py
+++ shanghai/main.py
@@ -1,6 +1,7 @@
 
 import asyncio
 from pprint import pprint
+import sys
 
 from .core import Shanghai
 from .config import Configuration
@@ -11,6 +12,37 @@ def exception_handler(loop, context):
     pprint(context)
     if 'task' in context:
         context['task'].print_stack()
+
+
+async def stdin_reader(loop, input_handler):
+    try:
+        if sys.platform == 'win32':
+            # Windows can't use SelectorEventLoop.connect_read_pipe
+            # and ProactorEventLoop.connect_read_pipe apparently
+            # doesn't work with sys.* streams or files.
+            # Instead, run polling in an executor (thread).
+            # http://stackoverflow.com/questions/31510190/aysncio-cannot-read-stdin-on-windows
+            while True:
+                line = await loop.run_in_executor(None, sys.stdin.readline)
+                if not line:
+                    break
+                loop.create_task(input_handler(line))
+        else:
+            reader = asyncio.StreamReader()
+            reader_protocol = asyncio.StreamReaderProtocol(reader)
+            await loop.connect_read_pipe(lambda: reader_protocol, sys.stdin)
+
+            while True:
+                line_bytes = await reader.readline()
+                line = line_bytes.decode(sys.stdin.encoding)
+                if not line:
+                    break
+                loop.create_task(input_handler(line))
+
+        print("stdin stream closed")
+    except:
+        import traceback
+        traceback.print_exc()
 
 
 def main():
@@ -27,8 +59,22 @@ def main():
     config = Configuration.from_filename('shanghai.yaml')
     bot = Shanghai(config)
     network_tasks = list(bot.init_networks())
+    print("networks:", ", ".join(bot.networks.keys()))
+
+    async def input_handler(line):
+        if ' ' not in line:
+            return
+        nw_name, irc_line = line.split(None, 1)
+        if nw_name and irc_line:
+            if nw_name not in bot.networks:
+                print("network '{}' not found".format(nw_name))
+                return
+            network = bot.networks[nw_name]['network']
+            network.connection.sendline(irc_line)
+
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
+    loop.create_task(stdin_reader(loop, input_handler))
     try:
         loop.run_until_complete(asyncio.wait(network_tasks, loop=loop))
     except KeyboardInterrupt:
